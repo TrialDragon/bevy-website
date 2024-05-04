bc385604882d3f073ef79217de6f0aaef396a068
TrialDragon <cj.2003@live.com>
2024-05-04 03:12:26 -0700

    Update generate community app to include profile pictures

--- generate-community/generate_community.sh
+++ generate-community/generate_community.sh
@@ -7,8 +7,9 @@ cd $(dirname $0)
 git clone --depth=1 https://github.com/bevyengine/bevy-community bevy-community
 
 # Generate data files for use in the people page.
-cargo run -- -i "bevy-community/Community Members/" -o community_members.toml
-cargo run -- -i "bevy-community/The Bevy Organization/" -o organization_members.toml
+cargo run -- -i "bevy-community/Community Members/" -o community_members.toml --pictures-output-folder ../content/community/people
+
+cargo run -- -i "bevy-community/The Bevy Organization/" -o organization_members.toml --pictures-output-folder ../content/community/people
 
 # Move the data files to the people page for use.
 mv community_members.toml organization_members.toml ../content/community/people/
--- generate-community/src/main.rs
+++ generate-community/src/main.rs
@@ -1,7 +1,7 @@
 use std::{fs, path::PathBuf};
 
 use clap::Parser;
-use generate_community::collect_member_files;
+use generate_community::{collect_member_files, copy_profile_pictures};
 
 /// Collects the TOML files
 /// containing info pertaining
@@ -9,6 +9,9 @@ use generate_community::collect_member_files;
 /// members into a single file
 /// for easier parsing by
 /// the template pages.
+///
+/// Also copies the relevant profile pictures
+/// to the output folder.
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
@@ -16,11 +19,15 @@ struct Args {
     /// TOML member files.
     #[arg(short, long)]
     input_folder: PathBuf,
-    /// File where the member files
+    /// Name of file the member files
     /// data will be collected
     /// for output.
     #[arg(short, long)]
     output_file: PathBuf,
+    /// Path to the pictures output
+    /// folder.
+    #[arg(long)]
+    pictures_output_folder: PathBuf,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -28,11 +35,16 @@ fn main() -> anyhow::Result<()> {
 
     println!("Collecting member files into a single file . . .");
     let member_file = collect_member_files(&args.input_folder)?;
+
     println!(
         "Outputting members file to {} . . .",
         args.output_file.to_string_lossy()
     );
-    fs::write(args.output_file, member_file)?;
+    fs::write(&args.output_file, member_file)?;
+
+    println!("Copying members' profile pictures to {} . . .", args.pictures_output_folder.to_string_lossy());
+    copy_profile_pictures(&args.input_folder, &args.pictures_output_folder)?;
+
 
     Ok(())
 }
